4c619710e22a2f85e63e5d5e13f85b3208aae0ee
pafadpkvfdlafjow <kayra.altun@elev.ga.ntig.se>
2026-06-03 09:14:54 +0200

    CSS klar

---
+++ js/script.js
@@ -0,0 +1,7 @@
+document.addEventListener("keydown", (event)=>{
+    console.log(event)
+    //console.log(document.getElementById(event.code).style.cssText)
+
+
+    //document.getElementById(event.code).style = "cssText.width background-color: red" 
+});
