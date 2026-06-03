9954be8490d8f9b058b2b434ea4ac23853402673
pafadpkvfdlafjow <kayra.altun@elev.ga.ntig.se>
2026-06-03 16:06:24 +0200

    text writing

--- js/script.js
+++ js/script.js
@@ -1,8 +1,30 @@
+var keyDowns = 0;
+var activated = false;
+const typeBar = document.getElementById("TypeBar"); 
 
+function StartRound(){
+    console.log("ITS STARTED")
+}
 
 document.addEventListener("keydown", (event)=>{
-    console.log(event)
     
+    if ((event.code.includes("Key") || 
+    event.code.includes("Digit") || 
+    event.code == "Space") && 
+     typeBar.textContent.length < 36){
+        typeBar.textContent += event.key;
+    }else if ((event.code === "Backspace")){
+        typeBar.textContent = typeBar.textContent.slice(0, -1);
+    };
+
+
+    if (!activated && keyDowns > 10){
+        activated = true;
+        StartRound();
+    };
+    
+    keyDowns += 1;
+
     if(event.code == "Enter"){
         const Enters = document.getElementsByClassName("Enter")
 
@@ -27,7 +49,7 @@ document.addEventListener("keyup", (event)=>{
         }
         
     } else{
-        document.getElementById(event.code).style.backgroundColor = "rgb(218, 218, 218)";
+        document.getElementById(event.code).style.backgroundColor = "";
         document.getElementById(event.code).style.transition = "all 0.3s";
     }
 });
