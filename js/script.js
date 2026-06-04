bf96d1ba9367e6c11b6dc5ae35c2bfbc8f405edd
pafadpkvfdlafjow <kayra.altun@elev.ga.ntig.se>
2026-06-04 08:45:21 +0200

    typing stuff

--- js/script.js
+++ js/script.js
@@ -1,31 +1,52 @@
-var keyDowns = 0;
 var activated = false;
 const typeBar = document.getElementById("TypeBar"); 
+const text = document.getElementById("Text"); 
+const promptThing = document.getElementById("Prompt"); 
+
+var promptThingLast = "";
 
 function StartRound(){
-    console.log("ITS STARTED")
+    console.log("ITS STARTED");
+    text.textContent = "";
+    typeBar.style.transition = "all 1s ease";
+    typeBar.style.width = "80%";
+    typeBar.style.visibility = "visible";
+
+    promptThing.textContent = "Robloxia";
+
 }
 
 document.addEventListener("keydown", (event)=>{
     var audio = new Audio("filler/popsound.mp3");
     audio.play();
 
+    if (!activated){
+        activated = true;
+        StartRound();
+        return;
+    };
+
     if ((event.code.includes("Key") || 
     event.code.includes("Digit") || 
     event.code == "Space") && 
-     typeBar.textContent.length < 36){
-        typeBar.textContent += event.key;
+     text.textContent.length < 36){
+        text.textContent += event.key;
+
+        if(promptThing.textContent[0] != undefined){
+            promptThingLast =  promptThing.textContent[0] + promptThingLast;
+        
+            promptThing.textContent = promptThing.textContent.slice(1);
+        }
     }else if ((event.code === "Backspace")){
-        typeBar.textContent = typeBar.textContent.slice(0, -1);
+        text.textContent = text.textContent.slice(0, -1);
+        
+        if(promptThingLast.length > text.textContent.length){
+            promptThing.textContent = promptThingLast[0] + promptThing.textContent
+
+            promptThingLast = promptThingLast.slice(1)
+        };
     };
 
-
-    if (!activated && keyDowns > 10){
-        activated = true;
-        StartRound();
-    };
-    
-    keyDowns += 1;
 
     if(event.code == "Enter"){
         const Enters = document.getElementsByClassName("Enter")
